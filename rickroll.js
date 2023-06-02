24c7d5871da287916e6cfd6bb5c07aedfd9f9aab
Quema <myoon0929@gmail.com>
2023-06-02 16:23:35 +0900

    fix?

--- rickroll.js
+++ rickroll.js
@@ -1,6 +1,6 @@
 let rickroll = () => {
     if (window.location.href.startsWith("https://www.youtube.com/")) {
-        const elements = document.querySelectorAll('*');
+        const elements = document.querySelectorAll('*,ytd-ad-slot-renderer.ytd-watch-next-secondary-results-renderer, ytd-compact-video-renderer.ytd-watch-next-secondary-results-renderer:not([expansion=collapsed]), ytd-compact-radio-renderer.ytd-watch-next-secondary-results-renderer, ytd-compact-movie-renderer.ytd-watch-next-secondary-results-renderer, ytd-compact-playlist-renderer.ytd-watch-next-secondary-results-renderer, ytd-compact-promoted-video-renderer.ytd-watch-next-secondary-results-renderer, ytd-promoted-sparkles-web-renderer.ytd-watch-next-secondary-results-renderer');
         elements.forEach((element) => {
             element.addEventListener('click', (e) => {
                 e.preventDefault();
@@ -9,11 +9,6 @@ let rickroll = () => {
                     e.preventDefault();
                     window.location.replace('https://youtu.be/dQw4w9WgXcQ');
                 }
-                window.addEventListener('beforeunload', () => {
-                    if (!window.location.href.endsWith('https://youtu.be/dQw4w9WgXcQ')) {
-                        window.location.replace('https://youtu.be/dQw4w9WgXcQ');
-                    }
-                });
             });
       });
     }
